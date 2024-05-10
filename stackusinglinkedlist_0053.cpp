c77b214d6ad7ef6c4b7cff8f5a3ba4d37bf738bf
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-10 14:50:13 +0700

    menulis if dan else

--- stackusinglinkedlist_0053.cpp
+++ stackusinglinkedlist_0053.cpp
@@ -40,11 +40,22 @@ public:
 		}
 		cout << "Popped value : " << top->data << endl;
 		top = top->next; // update the top pointer to the next node
+
 	}
 
 	// peek / top operation : retrive the value of the topmost element without removing it
 	void peek() {
-
+		if (top == NULL) {
+			cout << "list is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}// return the value of the top node
 	}
 };
 
