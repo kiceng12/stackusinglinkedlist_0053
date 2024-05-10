b0c9e9177a901814796ab72df1d60104289e419c
kiceng12 <sukmahendrawan12@gmail.com>
2024-05-10 15:11:42 +0700

    case 2

--- stackusinglinkedlist_0053.cpp
+++ stackusinglinkedlist_0053.cpp
@@ -85,6 +85,14 @@ int main() {
 			cin >> value;
 			stack.push(value); // push the entered value onto the stack
 			break;
+		case 2:
+			if (!stack.isEmpty()) {
+				stack.pop(); // Pop the top element from the stack
+			}
+			else {
+				cout << "Stack is empty. Cnnot pop." << endl;
+			}
+			break;
 		}
 
 
